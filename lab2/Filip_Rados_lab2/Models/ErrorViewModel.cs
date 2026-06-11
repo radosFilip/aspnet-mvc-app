b21464f51660dbb973eedd00a490bacccf0433f2
radosFilip <filiprados38@gmail.com>
2026-06-11 17:50:58 +0200

    Delete lab2 directory

--- lab2/Filip_Rados_lab2/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Filip_Rados_lab2.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
